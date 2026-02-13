45adbfa9e220420e0963fa3906c46beca70078c5
DaftFunk3737 <jasonfunkas@gmail.com>
2026-02-12 22:43:34 -0700

    feat: add header and footer to app component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+      <header className="container-fluid">
+        <nav className="navbar fixed-top navbar-dark">
+          <div className="navbar-brand">
+            Simon<sup>&reg;</sup>
+          </div>
+          <menu className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="login.html">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="play.html">
+                Play
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="scores.html">
+                Scores
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="about.html">
+                About
+              </a>
+            </li>
+          </menu>
+        </nav>
+      </header>
+
+      <main>App components go here</main>
+
+      <footer className="bg-dark text-white-50">
+        <div className="container-fluid">
+          <span className="text-reset">Your Name</span>
+          <a className="text-reset" href="https://github.com/DaftFunk123/simon-css">
+            Source
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
